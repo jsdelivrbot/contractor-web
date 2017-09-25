235aaf21ad81f08973d597deafa1e876feca6ae0
Sergiy Koyev <Sergiy.Koyev@capco.com>
2017-09-25 16:41:44 -0400

    Commit

--- index.js
+++ index.js
@@ -20,7 +20,24 @@ app.get('/', function(request, response) {
 
 // TODO: temp GET api
 app.get('/api/auth', function (req, res) {
-  res.status(200).json({message:'Tes'});
+  res.status(200).json({message:'Test'});
+});
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
 app.listen(app.get('port'), function() {
