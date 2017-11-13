e869bd381e8311810c882ef9e03ece05ece05f06
Sergiy Koyev <Sergiy.Koyev@capco.com>
2017-11-12 22:35:02 -0500

    Updated

--- modules/db-service.js
+++ modules/db-service.js
@@ -2,7 +2,7 @@
 
 module.exports = (function() {
     var self = this;
-    
+
     /**
      *
      */
@@ -10,7 +10,7 @@ module.exports = (function() {
         var appConst = require('./../routes/constants.js');
         var TimerJob = require('timer-jobs');
         var pgDb     = require('pg');
-        var self._        = require("underscore");
+        var _        = require("underscore");
 
         var dbCleanUpTimer = new TimerJob({interval: appConst.TIMER.DB_CLEAN_UP},
           function(done) {
@@ -22,7 +22,7 @@ module.exports = (function() {
 
                 client.query( appConst.QUERY.FIND_USER_TOKEN, [],
                              function(err, result) {
-                               self._.each(result.rows, function(row){
+                               _.each(result.rows, function(row){
                                  console.log(row.id);
                                });
                              });
