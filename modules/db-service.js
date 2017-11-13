05ef95a467e4155ca4b678a3b87b9b7d407488e8
Sergiy Koyev <Sergiy.Koyev@capco.com>
2017-11-12 22:26:26 -0500

    Updated

--- modules/db-service.js
+++ modules/db-service.js
@@ -6,11 +6,28 @@ module.exports = (function() {
      *
      */
     var startJobTokenCleanUp = function() {
+        var appConst = require('./constants.js');
         var TimerJob = require('timer-jobs');
-        var dbCleanUpTimer = new TimerJob({interval: 5000}, function(done) {
-          console.log('timer job');
-          done();
-        });
+        var pgDb     = require('pg');
+        var _        = require("underscore");
+
+        var dbCleanUpTimer = new TimerJob({interval: appConst.TIMER.DB_CLEAN_UP},
+          function(done) {
+            done();
+            pgDb.connect(appConst.DB_CONNECT_URI, function(err, client, done) {
+                if(err) {
+                  return;
+                }
+
+                client.query( self.const.QUERY.FIND_USER_TOKEN, [],
+                             function(err, result) {
+                               self._.each(result.rows, function(row){
+                                 console.log(row.id);
+                               });
+                             });
+            }
+          });
+
         dbCleanUpTimer.start();
     }
 
--- routes/constants.js
+++ routes/constants.js
@@ -6,6 +6,9 @@ module.exports = Object.freeze({
     JWT_REFRESH_TOKEN_SECRET: 'supersecret_refresh_token',
     REFRESH_TOKEN_EXPIRY_TIME_IN_HOURS: 60*60*3, // 3 hours expiry time
     ACCESS_TOKEN_EXPIRY_TIME_IN_SEC: 300, // 5 min expiry time
+    TIMER:{
+      DB_CLEAN_UP: 50000
+    },
     ERROR_CODE: {
     	LOGIN_INVALID: 10, // User login failed
     	LOGIN_FORM_INVALID: 11, // Login form invalid
@@ -19,6 +22,7 @@ module.exports = Object.freeze({
     QUERY: {
         FETCH_USERS: 'SELECT * FROM users',
         AUTH_UPDATE_USER_TOKEN: 'UPDATE user_token SET is_active = $1 WHERE token = $2',
-        NEW_USER_TOKEN: 'INSERT INTO user_token (id, user_id, token, is_active) VALUES (nextval($1), $2, $3, $4)'
+        NEW_USER_TOKEN: 'INSERT INTO user_token (id, user_id, token, is_active) VALUES (nextval($1), $2, $3, $4)',
+        FIND_USER_TOKEN: 'SELECT id from user_token'
     }
 });
