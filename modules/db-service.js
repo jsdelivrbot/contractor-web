bed24ba12432241b4e6e9ea27190b8b0500f92af
Sergiy Koyev <Sergiy.Koyev@capco.com>
2017-12-07 16:39:31 -0500

    Updated

--- modules/db-service.js
+++ modules/db-service.js
@@ -4,7 +4,7 @@ module.exports = (function() {
     var self = this;
 
     /**
-     *
+     * Token clean up job.
      */
     var startJobTokenCleanUp = function() {
         var appConst = require('./../routes/constants.js');
