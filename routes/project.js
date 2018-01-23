2346b3c2e059ff6bfa889a38bc9afbecced44d62
Sergiy Koyev <sergiykoyev@SergiyK-MacBook-Pro.local>
2018-01-23 10:11:19 -0500

    Updated

--- routes/project.js
+++ routes/project.js
@@ -33,7 +33,7 @@ var ProjectRouter = function() {
 
         self.base.executeQuery(self.const.QUERY.FETCH_PROJECTS, [])
                          .then(function(data){
-                             defer.resolve({data:data.rows, status: self.const.SUCCESS});
+                             defer.resolve({data:data.rows.data[0], status: self.const.SUCCESS});
                          }, function(error){
                            defer.reject(new Error(error));
                          });
